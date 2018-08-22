627a1f29381399d38d464fc371baec06452b7442
chena11356 <chena@bxscience.edu>
2018-08-22 18:50:10 -0400

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+function main(){
+  var app = new Vue({
+    el: '#test',
+    data: {
+      message: 'Hello, Vue!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  })
+}
+
+window.onload = main();
