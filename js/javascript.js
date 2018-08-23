09c8ba08447986c40a518c8d335e3d882f235032
chena11356 <chena@bxscience.edu>
2018-08-23 15:04:37 -0400

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -10,6 +10,20 @@ function main(){
       }
     }
   })
+
+  document.querySelector("a").addEventListener("click",function(e){
+    // dynamically determining the height of your navbar
+    let navbar = document.querySelector("nav");
+    let navbarheight = parseInt(window.getComputedStyle(navbar).height,10);
+    // show 5 pixels of previous section just for illustration purposes
+    let scrollHeight = document.querySelector(e.target.hash).offsetTop - navbarheight - 5;
+    /* scrolling to the element taking the height of the static bar into account*/
+    window.scroll(0,scrollHeight);
+    /*properly updating the window location*/
+    window.location.hash = e.target.hash;
+    /* do not execute default action*/
+    e.preventDefault();
+});
 }
 
 window.onload = main();
