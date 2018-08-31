79366802a9ec7ab43da3ffece77b06e03894e8b4
chena11356 <chena@bxscience.edu>
2018-08-30 21:03:19 -0400

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -498,11 +498,11 @@ function saveApp(){
   var totalService = 0;
   var totalLeadership = 0;
   for (var b = 1; b<=5; b++){
-    totalService += parseInt(document.getElementById("creditInput").value+b);
+    totalService += parseInt(document.getElementById("creditInput"+b).value);
   }
   document.getElementById("serviceInput").innerHTML = totalService+"";
   for (var b = 1; b<=10; b++){
-    totalLeadership += parseInt(document.getElementById("lcreditInput").value+b);
+    totalLeadership += parseInt(document.getElementById("lcreditInput"+b).value);
   }
   document.getElementById("leadershipInput").innerHTML = totalLeadership+"";
 
